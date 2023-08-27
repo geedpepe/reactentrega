5ad3514b4221e6ffbc7a718e865eeb4720f1a6a5
geedpepe <82737456+geedpepe@users.noreply.github.com>
2023-08-27 15:28:54 -0300

    2entrega

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -3,6 +3,12 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import mesa from "../images/mesa.jpg";
 import sofa from "../images/sofa.jpg";
+import Rack from "../images/rack.jpg";
+import Aparador from "../images/comoda.jpg";
+import Silla from "../images/silla.jpg";
+import Placard from "../images/placard.jpg";
+import Lampara from "../images/lampara pie.jpg";
+import mratona from "../images/mesa ratona.jpg";
 import './cards.css';
 
 
@@ -12,12 +18,12 @@ function Cards() {
   const products = [
     { id: 1, name: 'Mesa', description: 'Mesa madera 120x60x75', imagen: mesa },
     { id: 2, name: 'Sofa', description: 'Sofá aquaclean 200x80', imagen: sofa },
-    { id: 3, name: 'Rack', description: 'Sofá aquaclean 200x80', imagen: sofa },
-    { id: 4, name: 'Aparador', description: 'Sofá aquaclean 200x80', imagen: sofa },
-    { id: 5, name: 'Mesa Ratona', description: 'Sofá aquaclean 200x80', imagen: sofa },
-    { id: 6, name: 'Silla', description: 'Sofá aquaclean 200x80', imagen: sofa },
-    { id: 7, name: 'Placard', description: 'Sofá aquaclean 200x80', imagen: sofa },
-    { id: 8, name: 'Lampara', description: 'Sofá aquaclean 200x80', imagen: sofa },
+    { id: 3, name: 'Rack', description: 'Sofá aquaclean 200x80', imagen: Rack },
+    { id: 4, name: 'Aparador', description: 'Sofá aquaclean 200x80', imagen: Aparador },
+    { id: 5, name: 'Mesa Ratona', description: 'Sofá aquaclean 200x80', imagen: mratona },
+    { id: 6, name: 'Silla', description: 'Sofá aquaclean 200x80', imagen: Silla },
+    { id: 7, name: 'Placard', description: 'Sofá aquaclean 200x80', imagen: Placard },
+    { id: 8, name: 'Lampara', description: 'Sofá aquaclean 200x80', imagen: Lampara },
     // ... otros productos ...
   ];
 
