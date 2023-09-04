7df718c79cc11c27d43ec359622a8754b0477c2e
geedpepe <82737456+geedpepe@users.noreply.github.com>
2023-09-04 16:22:35 -0300

    funcionando

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import Card from './Components/ItemListContainer';
+import NavBar from '../src/Components/NavBar'; 
 
 function ColorSchemesExample() {
   const [cartItems, setCartItems] = useState([]);
@@ -17,16 +16,7 @@ function ColorSchemesExample() {
 
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Tienda Muebles</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Galeria</Nav.Link>
-            <Nav.Link href="#pricing">Trabajos</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <NavBar /> 
       <Container>
         <h1>Destacados</h1>
         <Card addToCart={addToCart} />
